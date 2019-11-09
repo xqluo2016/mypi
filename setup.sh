a7dbbe188e9a44e6bd3b7a808dd8b6db8f4bf61d
luoxq <xq.luo@foxmail.com>
2019-11-09 23:16:35 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,6 +12,18 @@ apt-get install -y xinetd
 
 
 echo "=========Setup SSL Proxy Bridge ============="
+
+function set_sys_proxy(){  
+	echo "Set system proxy local proxy"
+	cat >>/etc/environment <<ENDL
+	export http_proxy="http://127.0.0.1:8888"
+	export https_proxy="http://127.0.0.1:8888"
+	export no_proxy="localhost,127.0.0.1"
+	ENDL
+	. /etc/environment
+	wget http://www.sample.com/
+}
+
 echo "*** Please enter your SSL proxy <ip:port>:"
 read PROXY
 if [ "$PROXY" != "" ]; then
@@ -20,8 +32,6 @@ cat >/bin/proxy <<ENDL
 #!/bin/bash
 /usr/bin/openssl s_client -connect $PROXY --quiet 2>/dev/null
 ENDL
-
-#############
 
 chmod +x /bin/proxy
 
@@ -43,35 +53,50 @@ ENDL
 /etc/init.d/xinetd restart
 netstat -anpl |grep 8888
 
-echo "Set system proxy local proxy"
-cat >>/etc/environment <<ENDL
-export http_proxy="http://127.0.0.1:8888"
-export https_proxy="http://127.0.0.1:8888"
-export no_proxy="localhost,127.0.0.1"
-ENDL
-. /etc/environment
-wget http://www.sample.com/
+read -p "Do you want to set system proxy? (y/n): " yn
+case $yn in
+        [Yy]* ) set_sys_proxy ;;
+        * ) echo "not to set sys proxy";;
+esac
+
+
 else
 echo "skip proxy setting"
 fi
 
+
+apt-get update --fix-missing
 
 echo "=========INSTALL: samba for file sharing ============"
 apt-get install -y samba samba-common-bin
 
 echo "=========INSTALL: exfat support ====================="
 apt-get install -y exfat-fuse exfat-utils
+
 echo "=========INSTALL: ntfs support ======================"
 apt-get install -y ntfs-3g
 
-echo "=========List disks ============="
-ls -l /dev/disk/by-partuuid
-#TODO mount disks
 
-#############
+echo "=========Mount And Share disks ============="
+DISKS=$(ls /dev/sd??)
+mkdir -p /share
+DIR=/share
 
-# echo "PARTUUID=$UUID  $DIR/$LABEL    auto    defaults,nofail,nobootwait 0       0" >> /etc/fstab
+for disk in $DISKS
+do
+UUID=$(blkid -s UUID -o value $disk)
+LABEL=$(blkid -s LABEL -o value $disk)
+LABEL=$(echo $LABEL|tr ' ' '_')
+mkdir -p $DIR/$LABEL
+mount $disk $DIR/$LABEL
+
+if [ "$(cat /etc/fstab |grep $UUID)" == "" ]; then
+echo "PARTUUID=$UUID  $DIR/$LABEL    auto    defaults,nofail,nobootwait 0       0" >> /etc/fstab
+fi
+
+if [ "$(cat /etc/samba/smb.conf|grep $LABEL)" == "" ]; then
 cat >> /etc/samba/smb.conf <<ENDL
+
 [$LABEL]
 Comment = Pi shared folder
 Path = /$DIR/$LABEL
@@ -84,14 +109,38 @@ Public = yes
 Guest ok = yes
 write list = pi
 ENDL
+fi
+
+done
+
+service smbd restart
 
 
-#############
 
-echo "=========INSTALL: cups for Printer =================="
+echo "=========INSTALL: cups for printer =================="
 apt-get install -y cups
+usermod -a -G lpadmin pi
+echo "CUPS admin will be available on port 631 publicly."
+
+if [ "$(/etc/cups/cupsd.conf |grep 'Listen localhost:631')" == "" ]; then
+sed -i 's/Listen localhost:631/Listen 631/g' /etc/cups/cupsd.conf
+sed -i '/<\/Location>/i \
+  Allow @local' /etc/cups/cupsd.conf
+fi
+
+systemctl restart cups.service
+
+echo "********************************************************"
+echo "****use the follwoing linkes to configure your printer**"
+ifconfig |grep inet |awk '{print "http://"$2":631/admin"}'
+echo "********************************************************"
+
+echo "=========Sharing Printer =================="
+EXIST=$(cat /etc/samba/smb.conf |grep '[printers]')
+if [ "$EXIST" == "" ]; then
 
 cat >> /etc/samba/smb.conf <<ENDL
+
 # CUPS printing.  
 [printers]
 comment = All Printers
@@ -110,20 +159,22 @@ path = /var/lib/samba/printers
 browseable = yes
 read only = no
 guest ok = no
+
 ENDL
 
-#############
+fi
 
-cat >/root/stopPrinting <<ENDL
+echo "Make printer useable to everyone..."
+sed -i 's/read only = yes/read only = no/g' /etc/samba/smb.conf
+sed -i 's/browseable = no/browseable = yes/g' /etc/samba/smb.conf
+sed -i 's/guest ok = no/guest ok = yes/g' /etc/samba/smb.conf
+
+echo "Set cancel all printing jobs on pi startup..."
+cat >/etc/init.d/stopPrinting <<ENDL
 #!/bin/sh
-lpq -a
-lprm HP_Deskjet_1000_J110_series -
-lpq -a
+/usr/bin/cancel
+exit 0
 ENDL
 
 #############
-
-
-
-
-
+echo "All Done"
